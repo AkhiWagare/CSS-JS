e34bcad8d2822a7d9bf0fc21eb5bdf4b61eeb110
Akhi <akhileshwagare1102@gmail.com>
2022-11-20 17:30:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function uploadDocs() {
+    var fileName = document.getElementById("choose-file").value.toLowerCase();
+    if (!fileName.endsWith('.pdf')) {
+        alert('Please upload pdf file only!');
+        return false;
+    }
+
+    // location.replace("./insert.html")
+}
+
+function signIn() {
+    var uname = document.getElementById("username").value;
+    var upass = document.getElementById("password").value;
+
+    if (uname === 'admin' && upass === 'admin123') {
+        location.replace("./upload-docs.html") // window.open("./upload-docs.html", target = "_self")
+    }
+}
